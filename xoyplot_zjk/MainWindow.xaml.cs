3876af5b49754e2f95a43cd216ab726ea3566810
zhoujunkun <903368887@qq.com>
2019-05-29 19:25:18 +0800

    测试显示正常

--- xoyplot_zjk/MainWindow.xaml.cs
+++ xoyplot_zjk/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using OxyPlot;
 using OxyPlot.Axes;
 using OxyPlot.Series;
@@ -24,123 +26,61 @@ namespace xoyplot_zjk
     /// </summary>
     public partial class MainWindow : Window
     {
-        public PlotModel plotModel { get; set; }
-
-        private readonly Task task;
-        private int refresh;
-        private string title;
-        private bool complete;
+        DispatcherTimer timer = new DispatcherTimer();
+        Random rnd = new Random();
+        double time = 0d;
         public MainWindow()
         {
-
-            this.Points = new List<DataPoint>();
-          
-            //plotModel.LegendBackground = OxyColor.FromArgb(200, 255, 255, 255);
-            // plotModel.LegendBorder = OxyColors.Black;
-            //plotModel.LegendOrientation = LegendOrientation.Horizontal;
-            //plotModel.LegendPlacement = LegendPlacement.Outside;
-            //plotModel.LegendPosition = LegendPosition.RightTop;
-            //
-            //var linerAxis = new LinearAxis();
-            //linerAxis.MajorStep = 1;
-            //linerAxis.MinorStep = 1;
-            //linerAxis.MajorGridlineThickness = 3;
-            //linerAxis.MinorTicklineColor = OxyColors.Gray;
-            //linerAxis.TicklineColor = OxyColors.Red;
-            //linerAxis.TickStyle = TickStyle.Crossing;
-            //plotModel.Axes.Add(linerAxis);
-
-            //var linearAxis2 = new LinearAxis();
-            //linearAxis2.MajorGridlineThickness = 3;
-            //linearAxis2.MinorGridlineThickness = 1;
-            //linearAxis2.MajorStep = 1;
-            //linearAxis2.MinorStep = 1;
-            //linearAxis2.MinorTicklineColor = OxyColors.Gray;
-            //linearAxis2.Position = AxisPosition.Bottom;
-            //linearAxis2.TicklineColor = OxyColors.Blue;
-            //plotModel.Axes.Add(linearAxis2);
-
-            //function series
-
-
-            var worker = new BackgroundWorker { WorkerSupportsCancellation = true };
-
-            worker.DoWork += (s, e) =>
-            {
-                double x = 0;
-                double y = 0;
-                while (!worker.CancellationPending)
-                {
-                    Title = "Plot updated: " + DateTime.Now;
-                    Points.Add(new DataPoint(x, Math.Sin(y)));
-                    // Change the refresh flag, this will trig InvalidatePlot() on the Plot control
-                    this.Refresh++;
-                   // lk_plot.InvalidateFlag++;
-                    y += 0.1;
-                    x += 1;
-                    //if(x>20)
-                    //{
-                    //    lk_Y.MinimumRange = x - 20;
-                    //}
-                    
-                    this.Refresh++;
-                    Thread.Sleep(100);
-                }
-            };
-            worker.RunWorkerAsync();
-            this.Closed += (s, e) => worker.CancelAsync();
-            DataContext = this;
-
+            timer.Interval = TimeSpan.FromMilliseconds(50);
+            timer.Tick += Timer_Tick;
+            timer.IsEnabled = true;
+            
         }
-
-        public void Close()
+        int remve_index = 0;
+        int range = 50;
+        private void Timer_Tick(object sender, EventArgs e)
         {
-            this.complete = true;
-            this.task.Wait();
-        }
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public IList<DataPoint> Points { get; set; }
-
-        public string Title
-        {
-            get
+            Data.Add(new DataPoint(time++, rnd.NextDouble() * 5));
+            if(Data.Count>100)
             {
-                return this.title;
+                Data.RemoveAt(0);
             }
-
-            set
+            if(time > range)
             {
-                this.title = value;
-                this.RaisePropertyChanged("Title");
+                lk_yaxi.Minimum = time - range;
+            }
+        }
+        public ObservableCollection<DataPoint> Data
+        {
+            get { return (ObservableCollection<DataPoint>)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for Data.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data", typeof(ObservableCollection<DataPoint>), typeof(MainWindow), new PropertyMetadata(new ObservableCollection<DataPoint>()));
+        private void CheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+            timer.IsEnabled = (sender as CheckBox)?.IsChecked ?? false;
+            if (timer.IsEnabled)
+            {
+                // Reset
+                time = 0d;
+                Data = new ObservableCollection<DataPoint>();
             }
         }
 
-        public int Refresh
+        private void btn_click_show(object sender, RoutedEventArgs e)
         {
-            get
-            {
-                return this.refresh;
-            }
-
-            set
-            {
-                if (this.refresh == value)
-                {
-                    return;
-                }
-
-                this.refresh = value;
-                this.RaisePropertyChanged("Refresh");
-            }
+            
+          //  Data.Clear();
+            Data.RemoveAt(1);
         }
-        protected void RaisePropertyChanged(string property)
+
+        private void btn_click_new(object sender, RoutedEventArgs e)
         {
-            var handler = this.PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(property));
-            }
+            // Reset
+            time = 0d;
+            Data = new ObservableCollection<DataPoint>();
         }
     }
 }
